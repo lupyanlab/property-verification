56bc2cc6d54c0369762d0039a9d765d0e7ce36e3
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-07-03 09:22:11 -0500

    Added by subject plots

--- models/cue_first/feat_type/accuracy.R
+++ models/cue_first/feat_type/accuracy.R
@@ -2,6 +2,14 @@ library(lme4)
 source("scripts/cue_first_data.R")
 source("scripts/contrasts.R")
 
+# Remove outlier subject
+# ----------------------
+# Error rate is 65%!!!
+cue_first <- filter(cue_first, subj_id != "MWPF129")
+
+
+# Create contrast variables
+# -------------------------
 cue_first <- recode_mask_type(cue_first)
 cue_first <- recode_feat_type(cue_first)
 
@@ -15,6 +23,15 @@ summary(feat_type_error_mod)
 
 # Simple effect (visual question only)
 # ------------------------------------
-visual_type_error_mod <- glmer(is_error ~ mask_c + (1|subj_id), family = binomial,
-                               data = filter(cue_first, feat_type == "visual"))
+cue_first_visual_only <- filter(cue_first, feat_type == "visual")
+visual_type_error_mod <- glmer(is_error ~ mask_c + (1|subj_id),
+                               family = binomial, data = cue_first_visual_only)
 summary(visual_type_error_mod)
+
+
+# Simple effect (nonvisual questions only)
+# ----------------------------------------
+cue_first_nonvisual_only <- filter(cue_first, feat_type == "nonvisual")
+nonvisual_type_error_mod <- glmer(is_error ~ mask_c + (1|subj_id),
+                                  family = binomial, data = cue_first_nonvisual_only)
+summary(nonvisual_type_error_mod)
---
+++ plots/cue_first/descriptives/subj.R
@@ -0,0 +1,39 @@
+library(ggplot2)
+library(scales)
+source("scripts/cue_first_data.R")
+
+subjs <- cue_first %>%
+  group_by(subj_id) %>% 
+  summarize(
+    rt = mean(rt, na.rm = T),
+    err = mean(is_error, na.rm = T)
+  ) %>% 
+  mutate(
+    rank_rt = rank(rt, ties.method = "random"),
+    rank_err = rank(err, ties.method = "random")
+  )
+
+subj_rts <- ggplot(subjs, aes(x = rank_rt, y = rt, label = subj_id, color = subj_id)) +
+  geom_point() +
+  geom_text(hjust = 0, vjust = -0.5, angle = 45, size = 4) +
+  coord_cartesian(xlim = c(0.5, 49), ylim = c(500, 1500)) +
+  scale_x_continuous("Subject (Ranked By RT)", breaks = c(1, seq(5, 45, by = 5), 46)) +
+  scale_y_continuous("Average RT (ms)") +
+  theme(legend.position = "none")
+subj_rts
+
+subj_err <- ggplot(subjs, aes(x = rank_err, y = err, label = subj_id, color = subj_id)) +
+  geom_point() +
+  geom_text(hjust = 0, vjust = -0.5, angle = 45, size = 4) +
+  coord_cartesian(xlim = c(0.5, 49), ylim = c(0.0, 0.75)) +
+  scale_x_continuous("Subject (Ranked By Error Rate)", breaks = c(1, seq(5, 45, by = 5), 46)) +
+  scale_y_continuous("Error Rate", label = percent) +
+  theme(legend.position = "none")
+subj_err
+
+# Notes
+# -----
+# MWPF129 is a massive outlier in terms of error
+
+ggsave("plots/cue_first/descriptives/subj-rts.png", subj_rts, width = 10, height = 6)
+ggsave("plots/cue_first/descriptives/subj-err.png", subj_err, width = 10, height = 6)
