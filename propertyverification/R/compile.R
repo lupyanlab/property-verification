5f446080a2644c36b2db26eeabff016f8174c2e4
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-10-28 20:38:12 -0500

    Improvements to compiler

--- propertyverification/R/compile.R
+++ propertyverification/R/compile.R
@@ -1,10 +1,17 @@
-library(plyr)
-library(dplyr)
 
-compile <- function(dir, key, headername) {
-  fnames <- list.files(dir, key, full.names=TRUE)
-  header <- read.table(paste0(dir,'/',headername), header=T, sep='\t')
-  header <- names(header)
-  ldply(fnames, read.table, sep='\t', header=F, col.names=header, 
-        row.names=NULL, stringsAsFactors=F)
+#' Load the data files in a directory.
+#'
+#' @param data_dir The directory of the data files, passed to `file.path`.
+#' @param regex_key Pattern passed to `list.files`. Matches are read.
+#' @param header_file A file containing the data column names. Assumed
+#'        to be in data_dir, unless it exists.
+#' @return dplyr::data_frame
+#' @export
+compile <- function(data_dir, regex_key, header_file) {
+  header_file <- ifelse(file.exists(header_file), header_file,
+                        file.path(data_dir, header_file))
+  header <- colnames(readr::read_tsv(header_file))
+
+  data_files <- list.files(data_dir, key, full.names = TRUE)
+  plyr::ldply(data_files, read_tsv, col_names = header)
 }
