cd22918d3ec3ee804958daa702631a1b0a4d617a
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-01-18 16:28:07 -0600

    use labtools instead of resources

--- experiment/labtools/dynamic_mask.py
+++ experiment/labtools/dynamic_mask.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
-import os
+import unipath
 from psychopy.visual import ImageStim
 
 class DynamicMask(object):
-    def __init__(self, loc, key, **kwargs):
+    def __init__(self, **kwargs):
         """
-        :param loc: path to mask files
-        :param key: str key to identify the correct set of masks
         :param **kwargs: args to pass to visual.ImageStim
         """
-        mask_files = [os.path.join(loc, fname) for fname in os.listdir(loc) \
-                         if fname.find(key) != -1]
-        self.masks = [ImageStim(image = msk, **kwargs) for msk in mask_files]
+        labtools_dir = unipath.Path(__file__).absolute().parent
+        mask_files = unipath.Path(labtools_dir, 'dynamic_mask').listdir('*.png')
+        self.masks = [ImageStim(image = str(pth), **kwargs) for pth in mask_files]
         self.cur_ix = 0
-    
+
     def draw(self):
         """ Draws a single mask """
         self.masks[self.cur_ix].draw()
         self.cur_ix = (self.cur_ix+1) % len(self.masks)
-    
+
     def setPos(self, pos):
         """ Change the position for all masks"""
         for mask in self.masks:
             mask.setPos(pos)
-    
+
     def reset(self):
         """ Reset the mask index counter """
         self.cur_ix = 0
