8d009290d264a5e13467a6aa325e477df8abf02e
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-01-22 14:53:46 -0600

    use invoke to check stimuli

---
+++ experiment/stimuli/tasks.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""Make sure the stimuli are as expected for running the experiment.
+
+Usage:
+    $ invoke cue_info cue_stats
+    # creates cue_info.csv and cue_stats.csv
+"""
+from __future__ import print_function
+
+from invoke import task
+from unipath import Path
+from pandas import read_csv, DataFrame, concat
+
+CUES_DIR = 'cues'
+CUE_INFO_CSV = 'cue_info.csv'
+CUE_STATS_CSV = 'cue_stats.csv'
+
+@task
+def cue_info():
+    cues = Path(CUES_DIR).listdir('*.wav', names_only=True)
+    cue_info = DataFrame({'cue_file': cues})
+    filename_re = r'(?P<cue>[a-z]+)_(?P<version>\d)\.wav'
+    filename_args = cue_info.cue_file.str.extract(filename_re)
+    cue_info = concat([cue_info, filename_args], axis=1)
+    cue_info.to_csv(CUE_INFO_CSV, index=False)
+
+@task
+def cue_stats():
+    from psychopy import sound
+    cues = Path(CUES_DIR).listdir('*.wav')  # with path
+    cue_stats_records = []
+    for cue in cues:
+        # psychopy chokes on unipath.Path object
+        sound_obj = sound.Sound(str(cue))
+        cue_duration = (cue.name, sound_obj.getDuration())
+        cue_stats_records.append(cue_duration)
+
+    cue_stats = DataFrame.from_records(
+        cue_stats_records,
+        columns=['cue_file', 'duration'],
+    )
+
+    cue_stats.to_csv(CUE_STATS_CSV, index=False)
+
+@task(cue_info)
+def verify():
+    propositions = read_csv('propositions.csv')
+    available_cues = read_csv(CUE_INFO_CSV).cue.tolist()
+
+    any_missing = False
+    msg = "cue {} not found in cue files"
+    for proposition_category in propositions.cue.unique():
+        if proposition_category not in available_cues:
+            print(msg.format(proposition_category))
+            any_missing = True
+
+    if not any_missing:
+        print("all cues present and accounted for")
