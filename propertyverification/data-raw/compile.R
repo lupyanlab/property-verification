a0392ea5b49a919da5279a2172d5704d2397289e
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-01-25 15:02:07 -0600

    use single script for compiling data

--- propertyverification/data-raw/compile.R
+++ propertyverification/data-raw/compile.R
@@ -1,7 +1,7 @@
 library(stringr)
 library(dplyr)
-
 library(devtools)
+
 load_all()
 
 compile_question_first <- function(overwrite = FALSE) {
@@ -15,71 +15,84 @@ compile_question_first <- function(overwrite = FALSE) {
 
   question_first <- rbind(first_run, second_run)
 
+  question_first <- tidy_property_verification_data(question_first)
+
+  use_data(question_first, overwrite = overwrite)
+
+  question_first
+}
+
+compile_cue_first <- function(overwrite = FALSE) {
+  cue_first <- compile("data-raw/cue_first/data/", regex_key = "MWPF1",
+                       headername = "_header.txt")
+  cue_first <- tidy_property_verification_data(cue_first)
+  use_data(cue_first, overwrite = overwrite)
+  cue_first
+}
+
+compile_property_verification <- function(overwrite = FALSE) {
+  cue_first <- compile_cue_first(overwrite = overwrite)
+  cue_first$exp_run <- 1
+  cue_first$exp <- "cue_first"
+
+  question_first <- compile_question_first(overwrite = overwrite)
+  question_first$exp <- "question_first"
+
+  property_verification <- rbind_list(cue_first, question_first)
+  use_data(property_verification, overwrite = overwrite)
+}
+
+tidy_property_verification_data <- function(frame) {
   # Rename response columns
   # -----------------------
   # There were two columns named response in the original experiment.
   # The first response column, generated when the trials were created,
   # corresponds to the correct_response. The second response column is
   # the participant's response on that trial.
-  question_first <- rename(question_first,
-                           correct_response = response,
-                           response = response.1)
+  frame <- rename(frame,
+                  correct_response = response,
+                  response = response.1)
 
   # Remove practice trials
   # ----------------------
-  question_first <- filter(question_first, block_ix != -1)
+  frame <- filter(frame, block_ix != -1)
 
   # Exclude RT on timeout trials
   # ----------------------------
-  question_first$rt <- with(question_first, ifelse(response == "timeout", NA, rt))
+  frame$rt <- with(frame, ifelse(response == "timeout", NA, rt))
 
   # Save raw RTs for investigating speed-accuracy tradeoff
   # ------------------------------------------------------
-  question_first$raw_rt <- question_first$rt
+  frame$raw_rt <- frame$rt
 
   # Exclude RTs on incorrect responses and timeout trials
   # -----------------------------------------------------
-  question_first$rt <- with(question_first, ifelse(is_correct == 0, NA, rt))
-
-  # # Correct the calculation of RT
-  # # -----------------------------
-  # # The RT timer didn't start until after the offset of the cue,
-  # # but really it should have started at the onset of the cue.
-  # # In order to correct for this coding error, the duration of each
-  # # cue file needs to be added to the measured RT.
-  # cue_durations <- read.csv("experiment/stimuli/cues/_cue_stats.csv")
-  # cue_durations$cue_dur <- cue_durations$cue_dur * 1000  # convert sec to ms
-  # question_first <- merge(question_first, cue_durations, all.x = TRUE)
-  # question_first$rt <- with(question_first, cue_dur + rt)
-
-  # On second thought, people were prevented from answering before the
-  # prompt, and some of the cues were pretty long (~800 msec) so the
-  # above is not truly a correct calculation of RT.
+  frame$rt <- with(frame, ifelse(is_correct == 0, NA, rt))
 
   # Exclude accuracy on timeout trials
   # ----------------------------------
-  question_first$is_correct <- with(question_first, ifelse(response == "timeout", NA, is_correct))
+  frame$is_correct <- with(frame, ifelse(response == "timeout", NA, is_correct))
 
   # Make a new column to code accuracy in terms of error
   # ----------------------------------------------------
-  question_first$is_error <- with(question_first, ifelse(is_correct == 0, 1, 0))
+  frame$is_error <- with(frame, ifelse(is_correct == 0, 1, 0))
 
   # Create a unique proposition identifier
   # -----------------------------------
-  question_first$question_slug <- question_first$question %>%
-    str_to_lower %>%
+  frame$question_slug <- frame$question %>%
+    str_to_lower() %>%
     str_replace_all(" ", "-") %>%
     str_replace("\\?", "")
-  question_first$proposition_id <- with(question_first, paste(question_slug, cue, sep = ":"))
+  frame$proposition_id <- with(frame, paste(question_slug, cue, sep = ":"))
 
   # Merge norming ratings
   # ---------------------
   norms <- read.csv("data-raw/norms/norms.csv")
-  question_first <- merge(question_first, norms, all.x = TRUE)
+  frame <- merge(frame, norms, all.x = TRUE)
 
   # Put the columns in the correct order
   # ------------------------------------
-  question_first <- question_first %>%
+  frame <- frame %>%
     select(subj_id, exp_run,
            block = block_ix, trial = trial_ix,
            cue, question, proposition_id,
@@ -95,5 +108,5 @@ compile_question_first <- function(overwrite = FALSE) {
            raw_rt) %>%
     arrange(exp_run, subj_id, block, trial)
 
-  use_data(question_first, overwrite = overwrite)
+  frame
 }
