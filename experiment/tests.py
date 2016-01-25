661b0960df1f832a7b418c54cdbba8d1fac42b5d
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-01-25 14:37:26 -0600

    test without writing to disk

--- experiment/tests.py
+++ experiment/tests.py
@@ -9,15 +9,7 @@ from run import Trials
 
 @pytest.fixture(scope='module', params=[100, 104, 108])
 def trials(request):
-    trials_csv = 'test_trials.csv'
-    trials = Trials.make(seed=request.param)
-    trials.write(trials_csv)
-
-    def remove_trials():
-        os.remove(trials_csv)
-    request.addfinalizer(remove_trials)
-
-    return trials
+    return Trials.make(seed=request.param)
 
 def test_proposition_ids_are_unique(trials):
     frame = trials.to_dataframe()
