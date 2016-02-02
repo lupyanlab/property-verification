e99b7fa38f499f82e9c75450d18c41bd770b49f6
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-02-02 15:22:32 -0600

    only export a single recode function

--- propertyverificationdata/R/recoders.R
+++ propertyverificationdata/R/recoders.R
@@ -1,48 +1,73 @@
 
-#' Recode mask_type variable
+#' Recode variables for models and plots.
 #' @export
+recode_property_verification_data <- function(frame) {
+  frame %>%
+    recode_mask_type %>%
+    recode_feat_type %>%
+    recode_trial_type %>%
+    recode_correct_response %>%
+    recode_exp %>%
+    recode_exp_run
+}
+
+#' Recode mask type variable
 recode_mask_type <- function(frame) {
   mask_type_map <- dplyr::data_frame(
     mask_type = c("nomask", "mask"),
     mask_c = c(-0.5, 0.5),
     mask_f = factor(c("nomask", "mask"), levels = c("nomask", "mask")))
-  dplyr::left_join(frame, mask_type_map)
+  try(frame <- dplyr::left_join(frame, mask_type_map))
+  frame
 }
 
-#' Recode feat_type variable
-#' @export
+
+#' Recode feature type variable.
 recode_feat_type <- function(frame) {
   feat_type_map <- dplyr::data_frame(
     feat_type = c("nonvisual", "visual"),
     feat_c = c(-0.5, 0.5),
     feat_label = c("Encyclopedic Knowledge", "Visual Knowledge"))
-  dplyr::left_join(frame, feat_type_map)
+  try(frame <- dplyr::left_join(frame, feat_type_map))
+  frame
 }
 
-#' Recode experiment name variable
-#' @export
-recode_exp <- function(frame) {
-  experiment_map <- dplyr::data_frame(
-    exp = c("cue_first", "question_first"),
-    exp_c = c(-0.5, 0.5))
-  dplyr::left_join(frame, experiment_map)
+#' Recode combination of feature and mask trial type.
+recode_trial_type <- function(frame) {
+  trial_type_map <- dplyr::data_frame(
+    feat_type = rep(c("nonvisual", "visual"), times = 2),
+    mask_type = rep(c("nomask", "mask"), each = 2),
+    trial_type = paste(feat_type, mask_type, sep = ":")
+  )
+  try(frame <- dplyr::left_join(frame, trial_type_map))
+  frame
 }
 
-#' Recode experiment run variable
-#' @export
+#' Recode experiment run variable.
 recode_exp_run <- function(frame) {
   exp_run_map <- dplyr::data_frame(
     exp_run = c(1, 2, 3),
     exp_run_label = c("First run", "Second run", "Third run")
   )
-  dplyr::left_join(frame, exp_run_map)
+  try(frame <- dplyr::left_join(frame, exp_run_map))
+  frame
 }
 
-#' @export
+#' Recode the truth of the proposition.
 recode_correct_response <- function(frame) {
   correct_response_map <- dplyr::data_frame(
     correct_response = c("no", "yes"),
     correct_response_c = c(-0.5, 0.5)
   )
-  dplyr::left_join(frame, correct_response_map)
+  try(frame <- dplyr::left_join(frame, correct_response_map))
+  frame
+}
+
+#' Recode experiment name variable
+recode_exp <- function(frame) {
+  experiment_map <- dplyr::data_frame(
+    exp = c("cue_first", "question_first"),
+    exp_c = c(-0.5, 0.5))
+  try(frame <- dplyr::left_join(frame, experiment_map))
+  frame
 }
