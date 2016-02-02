2741d29c958e1b6df5fd3935b9e1fb535a872bef
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-02-01 19:03:42 -0600

    wrote function to calculate outliers

--- propertyverificationdata/R/filters.R
+++ propertyverificationdata/R/filters.R
@@ -1,3 +1,28 @@
+#' Label the outlier subjects in the sample.
+#'
+#' @import dplyr
+#' @importFrom broom tidy
+#' @export
+label_outlier_subjects <- function(frame) {
+  data(question_first)
+
+  subj_mods <- question_first %>%
+    tidy_property_verification_data %>%
+    recode_feat_type %>%
+    recode_mask_type %>%
+    group_by(subj_id) %>%
+    do(error_mod = glm(is_error ~ feat_c * mask_c, family = binomial, data = .))
+
+  subj_effects <- subj_mods %>%
+    tidy(error_mod) %>%
+    ungroup
+
+  outlier_map <- subj_effects %>%
+    group_by(subj_id) %>%
+    summarize(outlier = any(abs(estimate) > 5))
+
+  frame %>% left_join(outlier_map)
+}
 
 #' Label the ambiguity of propositions based on norming data.
 #'
